217c013904556eaf51cf468a0b53fbf1ddf75a20
Khuong <s3924577@rmit.edu.vn>
2022-08-04 11:36:15 +0700

    fin

--- src/Problem1.java
+++ src/Problem1.java
@@ -14,15 +14,32 @@ public class Problem1 {
     }
 
     public static boolean isFollowingRealRule(double[] locations, boolean[] Protected, double min){
-        for (int i = 0; i < locations.length - 1; i++){
-            if (locations[i+1] - locations[i] < min && !Protected[i+1]) return false;
+        double preLocation = locations[0];
+        for (int i = 1; i < locations.length; i++){
+            if (locations[i] - preLocation < min && !Protected[i]) return false;
+            if (!Protected[i]) preLocation = locations[i];
         }
         return true;
     }
 
     public static void main(String[] args) {
+        // method 1
         System.out.println(canFollowRule(9, 6, 2));
+        System.out.println(canFollowRule(10.0, 6, 2.0));
+        System.out.println(canFollowRule(10.0, 7, 2.0));
+        System.out.println(canFollowRule(10.0, 6, 2.5));
+        System.out.println(canFollowRule(0.1, 1, 0.0001));
+
+        // method 2
         System.out.println(isFollowingRule(new double[]{2, 3, 4, 5, 7, 9}, 2.021));
+        System.out.println(isFollowingRule(new double[]{0.0, 2.1, 4.5, 10.0}, 2.0));
+        System.out.println(isFollowingRule(new double[]{0.0, 1.5, 4.5, 10.0}, 2.0));
+        System.out.println(isFollowingRule(new double[]{-10.0, -7.5, 0.0}, 2.4));
+
+        // method 3
         System.out.println(isFollowingRealRule(new double[]{2, 3, 4, 5, 7, 9}, new boolean[]{true, false, true, false, false, false}, 3.21));
+        System.out.println(isFollowingRealRule(new double[]{0.0, 1.5, 4.5, 10.0}, new boolean[]{false, true, false, false}, 2.0));
+        System.out.println(isFollowingRealRule(new double[]{0.0, 1.5, 1.9, 10.0}, new boolean[]{false, true, false, false}, 2.0));
+        System.out.println(isFollowingRealRule(new double[]{0.0, 0.5, 1.0, 1.5, 2.1}, new boolean[]{false, true, true, true, false}, 2.0));
     }
 }
